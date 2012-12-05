e12a33cf4c641fb3e1b9bb90071ca18ad323a476
deepak <de_best4u@yahoo.co.in>
2012-12-05 13:52:27 +0530

    test

---
+++ MvcApplication2/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcApplication2
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{myWebForms}.aspx/{*pathInfo}");
+            routes.IgnoreRoute("{*path}", new { path = @"SimClockClientApp\/(.*)" });
+            routes.IgnoreRoute("{favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+            routes.IgnoreRoute("{robots}", new { favicon = @"(.*/)?robots.txt(/.*)?" });
+
+            routes.MapRoute(
+                "Default",                                                  // Route name
+                "{controller}/{action}/{id}",                               // URL with parameters
+                new { controller = "Home", action = "About", id = "" }   // Parameter defaults
+            );
+
+        }
+
+
+        protected void Application_Start()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //AuthConfig.RegisterAuth();
+
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+        }
+    }
+}
